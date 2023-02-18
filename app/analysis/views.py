d020dbb60d2df66d6ff746550d05f492f3c0a32b
e-kondr01 <e.kondr01@gmail.com>
2023-02-18 23:07:11 +0300

    feat: accept region id

--- app/analysis/views.py
+++ app/analysis/views.py
@@ -1,17 +1,20 @@
 from os.path import basename
 
+from django.shortcuts import get_object_or_404
 from drf_spectacular.utils import OpenApiParameter, extend_schema
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from analysis.plot_region_analysis import main as analyze_region
+from geography.models import Region
 
 
 class RegionAnalysisAPIView(APIView):
-    @extend_schema(parameters=[OpenApiParameter("region", type=str, required=True)])
+    @extend_schema(parameters=[OpenApiParameter("region_id", type=int, required=True)])
     def get(self, request, *_args, **_kwargs):
-        region = request.query_params.get("region")
-        resp = analyze_region(region)
+        region_id = request.query_params.get("region")
+        region_name = get_object_or_404(Region, id=region_id).name
+        resp = analyze_region(region_name)
         for region in resp:
             region[
                 "plot"
