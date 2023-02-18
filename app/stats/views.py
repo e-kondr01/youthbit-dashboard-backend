2fb2da5a7e24b3aaf6780bd1e6a2f93c63d7af22
e-kondr01 <e.kondr01@gmail.com>
2023-02-18 18:47:55 +0300

    fix: drf spectacular error

--- app/stats/views.py
+++ app/stats/views.py
@@ -1,4 +1,4 @@
-from django.db.models import Max, Min, QuerySet
+from django.db.models import Max, Min
 from rest_framework.generics import ListAPIView
 
 from stats.models import Feature, FeatureValue
@@ -28,14 +28,22 @@ class MapFeatureValueListView(ListAPIView):
     serializer_class = MapFeatureValueSerailizer
     filterset_fields = ("feature",)
 
-    def get_serializer(self, queryset: QuerySet, **kwargs):
-        max_value = queryset.aggregate(Max("value"))["value__max"]
-        min_value = queryset.aggregate(Min("value"))["value__min"]
-        serializer_class = self.get_serializer_class()
-        context = self.get_serializer_context()
-        context = context | {"max_value": max_value, "min_value": min_value}
-        kwargs.setdefault("context", context)
-        return serializer_class(queryset, **kwargs)
+    def __init__(self, **kwargs) -> None:
+        self.max_value = None
+        self.min_value = None
+        super().__init__(**kwargs)
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+        self.max_value = queryset.aggregate(Max("value"))["value__max"]
+        self.min_value = queryset.aggregate(Min("value"))["value__min"]
+        return super().list(request, *args, **kwargs)
+
+    def get_serializer_context(self):
+        return super().get_serializer_context() | {
+            "max_value": self.max_value,
+            "min_value": self.min_value,
+        }
 
 
 class ChildFeatureValueListView(ListAPIView):
