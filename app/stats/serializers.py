5f9400fd65a8108203866251ba33b66d6836132f
e-kondr01 <e.kondr01@gmail.com>
2023-02-18 16:05:25 +0300

    feat: add dummy color

--- app/stats/serializers.py
+++ app/stats/serializers.py
@@ -13,10 +13,14 @@ class FeatureSerializer(serializers.ModelSerializer):
 
 class MapFeatureValueSerailizer(serializers.ModelSerializer):
     region_code = serializers.CharField(source="region.code")
+    color = serializers.SerializerMethodField()
+
+    def get_color(self, obj):
+        return "#000000"
 
     class Meta:
         model = FeatureValue
-        fields = ("id", "value", "region_code")
+        fields = ("id", "value", "region_code", "color")
 
 
 class ChildFeatureValueSerializer(serializers.ModelSerializer):
